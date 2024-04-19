efb081fcf78c2ab437abdee2bbb1abb3026f07fe
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-19 22:04:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import requests
 from pyrogram import Client
-from pyrogram.types import Message
 
 # Your bot's API credentials
 api_id = "10247139"
@@ -11,24 +10,32 @@ bot_token = '6461840799:AAG5Ve1YeSkR1fEQfsI3plN51_oqCm25G9U'
 # The channel ID where the bot will send messages
 channel_id = -1002022217944
 
+# Variable to track website status (initially set to 'up')
+website_status = 'up'
+
 # Function to check website status
 async def check_website_status():
+    global website_status
     while True:
         try:
-            # Replace 'https://hianime.to' with the URL you want to monitor
+            # Replace 'https://ddl.animxt.fun' with the URL you want to monitor
             response = requests.get('https://ddl.animxt.fun')
             if response.status_code != 200:
-                error_message = f"Website is down! Error code: {response.status_code}"
-                await bot.send_message(channel_id, error_message)
+                if website_status == 'up':
+                    error_message = f"Website is down! Error code: {response.status_code}"
+                    await bot.send_message(channel_id, error_message)
+                    website_status = 'down'
             else:
-                await bot.send_message(channel_id, "Website is back up!")
+                if website_status == 'down':
+                    await bot.send_message(channel_id, "Website is back up!")
+                    website_status = 'up'
 
         except Exception as e:
             error_message = f"An error occurred: {str(e)}"
             await bot.send_message(channel_id, error_message)
 
         # Check every 5 minutes (adjust the sleep duration as needed)
-        await asyncio.sleep(30)
+        await asyncio.sleep(300)
 
 # Create the bot client
 bot = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
@@ -40,5 +47,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
